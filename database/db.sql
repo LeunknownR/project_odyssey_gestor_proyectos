6a0eb4ae76b709fbc094ba8335a15cf67b0c3348
Torresito__ <diegoteodosio@gmail.com>
2023-05-10 09:45:17 -0500

    creación de las tablas necesarias del sprint 1

--- database/db.sql
+++ database/db.sql
@@ -32,7 +32,7 @@ CREATE TABLE `user` (
     `user_surname` VARCHAR(50) NOT NULL,
     `username` VARCHAR(12) NOT NULL,
     `userpassword` VARCHAR(60) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
     `active` BIT NOT NULL DEFAULT 1,
     `id_role` INT UNSIGNED NOT NULL,
     UNIQUE KEY (`email`),
@@ -95,54 +95,9 @@ CREATE TABLE `members_project` (
     FOREIGN KEY (`id_project_role`) REFERENCES `project_role`(`id_project_role`)
 );
 
--- Tabla para guardar las tareas
-DROP TABLE IF EXISTS `task`;
-CREATE TABLE `task` (
-    `id_task` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `task_name` VARCHAR(255) NOT NULL,
-    `description` TEXT,
-    `deadline` DATE,
-    `status` ENUM('not_started', 'in_progress', 'completed') NOT NULL DEFAULT 'not_started',
-    `id_project` INT UNSIGNED NOT NULL,
-    `id_members_project` INT UNSIGNED,
-    -- Por si no esta asignada aun al members_proyect - colaborator - user [ID_MEMBERS_PROJECT]
-    PRIMARY KEY (`id_task`),
-    FOREIGN KEY (`id_project`) REFERENCES `project`(`id_project`),
-    FOREIGN KEY (`id_members_project`) REFERENCES `members_project`(`id_members_project`)
-);
-
--- Tabla para guardar las subtareas de cada tarea
-DROP TABLE IF EXISTS `subtasks`;
-CREATE TABLE `subtasks` (
-    `id_subtasks` INT UNSIGNED AUTO_INCREMENT,
-    `subtasks_name` VARCHAR(255) NOT NULL,
-    `id_task` INT UNSIGNED NOT NULL,
-    PRIMARY KEY (`id_subtasks`),
-    FOREIGN KEY (`id_task`) REFERENCES `task`(`id_task`)
-);
-
--- Tabla para guardar las etiquetas
-DROP TABLE IF EXISTS `label`;
-CREATE TABLE `label` (
-    `id_label` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `label_name` VARCHAR(255) NOT NULL,
-    `color` VARCHAR(7),
-    PRIMARY KEY (`id_label`)
-);
-
--- Tabla de relación entre tareas y etiquetas
-DROP TABLE IF EXISTS `task_label`;
-CREATE TABLE `task_label` (
-    `id_task` INT UNSIGNED NOT NULL,
-    `id_label` INT UNSIGNED NOT NULL,
-    PRIMARY KEY (`id_task`, `id_label`),
-    FOREIGN KEY (`id_task`) REFERENCES `task`(`id_task`),
-    FOREIGN KEY (`id_label`) REFERENCES `label`(`id_label`)
-);
 
 
-
--- INSERT INTO --
+-- [ INSERT INTO ] --
 INSERT INTO user(
     id_user,
     user_name,
@@ -154,11 +109,15 @@ INSERT INTO user(
     id_role
 ) 
     VALUES
-    (1, "Diego Edgardo", "Torres De La Cruz", "diegot", "diegot123", "diegoteodosiof@gmail.com", 1, 1),
-    (2, "Ralf Carsten", "Carrasco Stein", "ralfc", "ralfc123", "ralfcarrasco@gmail.com", 1, 2);
+    (1, "Diego Edgardo", "Torres De La Cruz", "diegot", "$2a$10$9dDabNh8viE00ZllRHhAA.3UEvpq/7XevLy472LijBO8kvOruajSa", "diegoteodosiof@gmail.com", 1, 1),
+    (2, "Ralf Carsten", "Carrasco Stein", "ralfc", "$2a$10$YkcmXbwu9NIIw7ek4x/UUuPZtwzEpvAa7N3hnMcg0bC2pK3/pxoaS", "ralfcarrasco@gmail.com", 1, 2),
+    (3, "Manuel Alejandro", "Rivera Becerra", "manuelr", "$2a$10$qon6KKzLiPUaMDfuaYQ0aeO.2yils9vOxsVkAlOouHakcNgSu6gxe", "leunknow@gmail.com", 1, 2);
 
 
--- STORED PROCEDUREs --
+
+-- [ STORED PROCEDUREs ] --
+
+-- Stored procedure para el login
 DELIMITER //
 CREATE PROCEDURE `get_userpassword_by_username`(
     IN p_username VARCHAR(20)
@@ -166,18 +125,16 @@ CREATE PROCEDURE `get_userpassword_by_username`(
 BEGIN
     -- Declarando variable para la userpassword
     DECLARE user_password VARCHAR(60) DEFAULT NULL;
-
     -- Extrayendo la contraseña respectiva al "username" y asignando la variable de userpassword
     SELECT userpassword 
         INTO user_password
     FROM user
     WHERE username = p_username 
         AND active = 1;
-
+    -- Condicional para verificar si se encontro un "userpassword"
     IF user_password IS NULL THEN
         SET user_password = NULL;
     END IF;
-
     -- Mostrando la userpass
     SELECT user_password AS `userpass`;
 END //
