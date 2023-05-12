c08717b6b52af9ae836a534ecfde8da95ad7c4e4
Torresito__ <diegoteodosio@gmail.com>
2023-05-12 02:21:28 -0500

    Update db

--- database/db.sql
+++ database/db.sql
@@ -11,18 +11,16 @@ USE project_odyssey_gestor_proyectos;
 -- Tabla para guardar el rol de los usuarios
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
-    `id_role` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `role_name` VARCHAR(50) NOT NULL,
+    `id_role` CHAR(3) NOT NULL,
     PRIMARY KEY (`id_role`)
 );
 -- Insertando valores a "role"
 INSERT INTO `role` (
-    `id_role`, 
-    `role_name`
+    `id_role`
 )
 VALUES 
-    (1, "Admin. General"),
-    (2, "Colaborator");
+    ("GAD"),
+    ("CLB");
 
 -- Tabla para guardar los usuarios
 DROP TABLE IF EXISTS `user`;
@@ -32,9 +30,10 @@ CREATE TABLE `user` (
     `user_surname` VARCHAR(50) NOT NULL,
     `username` VARCHAR(12) NOT NULL,
     `userpassword` VARCHAR(60) NOT NULL,
+    `url_photo` VARCHAR(100) DEFAULT NULL,
     `email` VARCHAR(50) NOT NULL,
     `active` BIT NOT NULL DEFAULT 1,
-    `id_role` INT UNSIGNED NOT NULL,
+    `id_role` CHAR(3) NOT NULL,
     UNIQUE KEY (`email`),
     PRIMARY KEY (`id_user`),
     FOREIGN KEY (`id_role`) REFERENCES `role`(`id_role`)
@@ -49,49 +48,51 @@ CREATE TABLE `admin_general` (
 );
 
 -- Tabla para guardar el ID del colaborator y sus datos
-DROP TABLE IF EXISTS `colaborator`;
-CREATE TABLE `colaborator` (
-    `id_colaborator` INT UNSIGNED NOT NULL,
-    `url_photo` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`id_colaborator`),
-    FOREIGN KEY (`id_colaborator`) REFERENCES `user`(`id_user`)
+DROP TABLE IF EXISTS `collaborator`;
+CREATE TABLE `collaborator` (
+    `id_collaborator` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id_collaborator`),
+    FOREIGN KEY (`id_collaborator`) REFERENCES `user`(`id_user`)
 );
 
 -- Tabla para guardar los proyectos
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
     `id_project` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `project_name` VARCHAR(255) NOT NULL,
-    `description` TEXT,
+    `project_name` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(200),
+    `creation_date` DATE NOT NULL,
+    `state` ENUM('O', 'F', 'P') NOT NULL,
     `start_datetime` DATETIME NOT NULL,
     `end_datetime` DATETIME NOT NULL,
-    PRIMARY KEY (`id_project`)
+    `active` BIT NOT NULL DEFAULT 1,
+    `id_admin_general` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id_project`),
+    FOREIGN KEY (`id_admin_general`) REFERENCES `admin_general`(`id_admin_general`)
 );
 
 -- Tabla para guardar los roles de los miembros dentro del proyecto
 DROP TABLE IF EXISTS `project_role`;
 CREATE TABLE `project_role` (
-    `id_project_role` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `project_role_name` VARCHAR(25) NOT NULL,
+    `id_project_role` CHAR(3) NOT NULL,
     PRIMARY KEY (`id_project_role`)
 );
 -- Insertando valores a "project_role"
 INSERT INTO `project_role` (
-    id_project_role, 
-    project_role_name
+    id_project_role
 )
 VALUES 
-    (1, "Líder del proyecto"),
-    (2, "Miembro");
+    ("PLD"),
+    ("PMB");
 
 -- Tabla para guardar los miembros del proyecto
 DROP TABLE IF EXISTS `members_project`;
 CREATE TABLE `members_project` (
     `id_members_project` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `id_colaborator` INT UNSIGNED NOT NULL,
-    `id_project_role` INT UNSIGNED NOT NULL,
+    `id_collaborator` INT UNSIGNED NOT NULL,
+    `id_project_role` CHAR(3) NOT NULL,
     PRIMARY KEY (`id_members_project`),
-    FOREIGN KEY (`id_colaborator`) REFERENCES `colaborator`(`id_colaborator`),
+    FOREIGN KEY (`id_collaborator`) REFERENCES `collaborator`(`id_collaborator`),
     FOREIGN KEY (`id_project_role`) REFERENCES `project_role`(`id_project_role`)
 );
 
@@ -108,29 +109,34 @@ INSERT INTO user(
     active,
     id_role
 ) 
-    VALUES
-    (1, "Diego Edgardo", "Torres De La Cruz", "diegot", "$2a$10$9dDabNh8viE00ZllRHhAA.3UEvpq/7XevLy472LijBO8kvOruajSa", "diegoteodosiof@gmail.com", 1, 1),
-    (2, "Ralf Carsten", "Carrasco Stein", "ralfc", "$2a$10$YkcmXbwu9NIIw7ek4x/UUuPZtwzEpvAa7N3hnMcg0bC2pK3/pxoaS", "ralfcarrasco@gmail.com", 1, 2),
-    (3, "Manuel Alejandro", "Rivera Becerra", "manuelr", "$2a$10$qon6KKzLiPUaMDfuaYQ0aeO.2yils9vOxsVkAlOouHakcNgSu6gxe", "leunknow@gmail.com", 1, 2);
+VALUES
+    (1, "Diego Edgardo", "Torres De La Cruz", "diegot", "$2a$10$9dDabNh8viE00ZllRHhAA.3UEvpq/7XevLy472LijBO8kvOruajSa", "diegoteodosiof@gmail.com", 1, "GAD"),
+    (2, "Ralf Carsten", "Carrasco Stein", "ralfc", "$2a$10$YkcmXbwu9NIIw7ek4x/UUuPZtwzEpvAa7N3hnMcg0bC2pK3/pxoaS", "ralfcarrasco@gmail.com", 1, "CLB"),
+    (3, "Manuel Alejandro", "Rivera Becerra", "manuelr", "$2a$10$qon6KKzLiPUaMDfuaYQ0aeO.2yils9vOxsVkAlOouHakcNgSu6gxe", "leunknownr@gmail.com", 1, "CLB");
 
-
+INSERT INTO colaborator(
+    id_collaborator
+)
+VALUES
+    (2),
+    (3);
 
 -- [ STORED PROCEDUREs ] --
 
--- Stored procedure para el login
+-- SP para el login
 DELIMITER //
-CREATE PROCEDURE `get_userpassword_by_username`(
+CREATE PROCEDURE `sp_get_userpassword_by_username`(
     IN p_username VARCHAR(20)
 )
 BEGIN
     -- Declarando variable para la userpassword
     DECLARE user_password VARCHAR(60) DEFAULT NULL;
     -- Extrayendo la contraseña respectiva al "username" y asignando la variable de userpassword
-    SELECT userpassword 
-        INTO user_password
+    SELECT userpassword
+    INTO user_password
     FROM user
     WHERE username = p_username 
-        AND active = 1;
+    AND active = 1;
     -- Condicional para verificar si se encontro un "userpassword"
     IF user_password IS NULL THEN
         SET user_password = NULL;
@@ -139,3 +145,113 @@ BEGIN
     SELECT user_password AS `userpass`;
 END //
 DELIMITER ;
+
+-- SP para traer la información basica de un user segun su username
+DELIMITER //
+CREATE PROCEDURE `sp_get_basic_user_information`(
+    IN p_username VARCHAR(20)
+)
+BEGIN
+    -- Trayendo la información cuando el usernema coincida
+    SELECT 
+        u.id_user, 
+        u.name, 
+        u.surname,  
+        u.username, 
+        u.url_photo,
+        u.id_role
+    FROM user u
+    JOIN role r ON u.id_role = r.id_role
+    WHERE u.username = p_username;
+END //
+DELIMITER ;
+
+/*¨OJO CON EL ACTIVE"*/
+-- sp_get_project_list_by_project_name(
+-- 	p_project_name
+-- );
+
+-- SP 
+DELIMITER //
+CREATE PROCEDURE `sp_search_collaborator_by_username`(
+    IN p_collaborator_name VARCHAR(50)
+)
+BEGIN
+    -- Trayendo registros
+    SELECT
+        u.user_name,
+        u.user_surname,
+        u.url_photo
+    FROM collaborator clb
+    INNER JOIN user u ON clb.id_collaborator = u.id_user 
+    WHERE u.name = p_collaborator_name;
+END //
+DELIMITER ;
+
+-- 
+DELIMITER //
+CREATE PROCEDURE `sp_create_project`(
+    IN p_id_admin_general INT,
+    IN p_project_name VARCHAR(50),
+    IN p_project_description VARCHAR(200),
+    IN p_project_start_date DATE,
+    IN p_project_end_date DATE,
+    IN p_id_collaborator INT
+)
+BEGIN
+    -- Validaciones previas a la creación del proyecto
+    IF EXISTS (
+        SELECT id_project
+        FROM project
+        WHERE project_name = p_project_name
+    ) THEN
+        -- Creación de un nuevo proyecto
+        INSERT INTO project (
+            project_name,
+            description,
+            creation_date,
+            state,
+            start_datetime,
+            end_datetime,
+            id_admin_general
+        ) VALUES (
+            p_project_name,
+            p_project_description,
+            NOW(),
+            'O',
+            p_project_start_date,
+            p_project_end_date,
+            p_id_admin_general
+        );
+
+        -- Seteando el ultimo id de proyecto insertado
+        SET @id_project = LAST_INSERT_ID();
+
+        -- Insertando el miembro que sera lider
+        INSERT INTO members_project (
+            id_project,
+            id_collaborator,
+            id_project_role
+        ) VALUES (
+            @id_project,
+            p_id_collaborator,
+            'PLD'
+        );
+    END IF;
+END //
+DELIMITER ;
+
+-- 
+-- DELIMITER //
+-- CREATE PROCEDURE `sp_update_project`(
+--     IN p_id_project INT,
+--     IN p_project_name VARCHAR(50),
+--     IN p_project_description VARCHAR(200),
+--     IN p_project_start_date DATE,
+--     IN p_project_end_date DATE.
+--     IN p_id_collaborator INT
+-- )
+-- BEGIN
+
+-- END //
+-- DELIMITER ;
