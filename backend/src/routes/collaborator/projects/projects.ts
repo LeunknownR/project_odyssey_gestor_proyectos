3db9fee07e7c54d519176b4acd811e0d1e5c1647
Manuel Rivera Becerra <leunknownr@gmail.com>
2023-05-15 14:05:31 -0500

    Avances backend     * Se integró captura de errores correctamente.     * Se agregó pepper para validar la contraseña en el login.     * Se validó funcionamiento del servicio de login.

--- backend/src/routes/collaborator/projects/projects.ts
+++ backend/src/routes/collaborator/projects/projects.ts
@@ -5,31 +5,36 @@ import { ApiPathEndpointsCollaborator } from "../../apiPaths";
 import { ResponseBody } from "../../../utils/types";
 import ProjectController from "../../../controllers/projectController/projectController";
 import { GenerateResponseBody } from "../../../utils/generateResponseBody";
-import { parseToAddProjectMembersRequestBody, parseToCollaboratorName, parseToProjectId, parseToSearchCollaboratorRequestBody, parseToUpdateEndDateProjectRequestBody } from "./parsers";
+import { parseToAddProjectMembersRequestBody, parseToSearchCollaboratorRequestBody, parseToUpdateEndDateProjectRequestBody } from "./parsers";
 import { parseToProjectName } from "../../generalAdmin/projects/parsers";
 import { AddProjectMembersRequestBody, SearchCollaboratorRequestBody, UpdateEndDateProjectRequestBody } from "../../../entities/project/types";
+import { withErrorHandler } from "../../helpers";
 
 const router = Router();
 router.use("/", Authentication.checkTokenInEndpoints(DBRoles.Collaborator));
-router.get(ApiPathEndpointsCollaborator.GetProjectListForCollaborator, async (req, res) => {
-    const projectName: string = parseToProjectName(req.params);
-    const payload: ResponseBody = await ProjectController.getProjectListForCollaborator(projectName);
-    GenerateResponseBody.sendResponse(res, payload);
-});
-router.patch(ApiPathEndpointsCollaborator.UpdateEndDateProject, async (req, res) => {
-    const updateEndDateProjectRequestBody: UpdateEndDateProjectRequestBody = parseToUpdateEndDateProjectRequestBody(req.body);
-    const payload: ResponseBody = await ProjectController.updateEndDateProjectByLeader(updateEndDateProjectRequestBody);
-    GenerateResponseBody.sendResponse(res, payload);
-});
-router.get(ApiPathEndpointsCollaborator.SearchCollaboratorMember, async (req, res) => {
-    const searchCollaboratorRequestBody: SearchCollaboratorRequestBody = parseToSearchCollaboratorRequestBody(req.params)
-    const payload: ResponseBody = await ProjectController.searchCollaboratorsMembersByLeader(searchCollaboratorRequestBody);
-    GenerateResponseBody.sendResponse(res, payload);
-});
-router.patch(ApiPathEndpointsCollaborator.AddProjectMembers, async (req, res) => {
-    const addProjectMembersRequestBody: AddProjectMembersRequestBody = parseToAddProjectMembersRequestBody(req.body);
-    const payload: ResponseBody = await ProjectController.addProjectMembers(addProjectMembersRequestBody);
-    GenerateResponseBody.sendResponse(res, payload);
-});
+router.get(ApiPathEndpointsCollaborator.GetProjectListForCollaborator,
+    withErrorHandler(async (req, res) => {
+        const projectName: string = parseToProjectName(req.params);
+        const payload: ResponseBody = await ProjectController.getProjectListForCollaborator(projectName);
+        GenerateResponseBody.sendResponse(res, payload);
+    }));
+router.patch(ApiPathEndpointsCollaborator.UpdateEndDateProject,
+    withErrorHandler(async (req, res) => {
+        const updateEndDateProjectRequestBody: UpdateEndDateProjectRequestBody = parseToUpdateEndDateProjectRequestBody(req.body);
+        const payload: ResponseBody = await ProjectController.updateEndDateProjectByLeader(updateEndDateProjectRequestBody);
+        GenerateResponseBody.sendResponse(res, payload);
+    }));
+router.get(ApiPathEndpointsCollaborator.SearchCollaboratorMember,
+    withErrorHandler(async (req, res) => {
+        const searchCollaboratorRequestBody: SearchCollaboratorRequestBody = parseToSearchCollaboratorRequestBody(req.params)
+        const payload: ResponseBody = await ProjectController.searchCollaboratorsMembersByLeader(searchCollaboratorRequestBody);
+        GenerateResponseBody.sendResponse(res, payload);
+    }));
+router.patch(ApiPathEndpointsCollaborator.AddProjectMembers,
+    withErrorHandler(async (req, res) => {
+        const addProjectMembersRequestBody: AddProjectMembersRequestBody = parseToAddProjectMembersRequestBody(req.body);
+        const payload: ResponseBody = await ProjectController.addProjectMembers(addProjectMembersRequestBody);
+        GenerateResponseBody.sendResponse(res, payload);
+    }));
 
 export default router;
