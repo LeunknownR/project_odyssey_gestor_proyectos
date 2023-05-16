6727d29eac31f5c098bccda1475142a6108dd9fd
Manuel Rivera Becerra <leunknownr@gmail.com>
2023-05-15 20:52:42 -0500

    Avances backend     * Se validaron servicios de:         - Listar proyectos para el admin. general.         - Crear proyecto.

--- backend/src/routes/generalAdmin/projects/parsers.ts
+++ backend/src/routes/generalAdmin/projects/parsers.ts
@@ -1,38 +1,55 @@
 import { ProjectForm } from "../../../entities/project/types";
 import { isPositiveNumber } from "../../../utils/numbers";
-import { isLimitMaximString, isString } from "../../../utils/string";
+import { checkMaxLength, isString } from "../../../utils/string";
 import { CreateProjectRequestBody } from "./types";
 
 export const parseToProjectName = (params: any): string => {
-    if (!isLimitMaximString(params.projectName,255))
+    if (!checkMaxLength(params.projectName, 255))
         throw new Error("Invalid project name");
     return params.projectName;
 }
 export const parseToCollaboratorName = (params: any): string => {
-    if (!isLimitMaximString(params.collaboratorName,100))
+    if (!checkMaxLength(params.collaboratorName, 100))
         throw new Error("Invalid collaborator name");
     return params.projectName;
 }
-export const parseToCreateProjectRequestBody = (body: any): CreateProjectRequestBody => {
+export const parseToProjectForm = (body: any, withId: boolean): ProjectForm => {
     const {
-        userId, name, description,
+        id, name, description,
         startDate, endDate, leaderId
     } = body;
-    if (!isPositiveNumber(userId) ||
-        !isLimitMaximString(name,255) || !isString(description) ||
-        !isPositiveNumber(startDate) || !isPositiveNumber(endDate) ||
+    if ((withId && !isPositiveNumber(id)) ||
+        !checkMaxLength(name, 255) || 
+        !checkMaxLength(description, 200)  ||
+        !isPositiveNumber(startDate) || 
+        !isPositiveNumber(endDate) ||
         !isPositiveNumber(leaderId))
-        throw new Error("Invalid form to create project");
+        throw new Error("Invalid project form");
+    const projectForm: ProjectForm = {
+        name,
+        description,
+        startDate,
+        endDate,
+        leaderId
+    };
+    if (withId)
+        projectForm.id = id;
+    return projectForm;
+}
+export const parseToCreateProjectRequestBody = (body: any): CreateProjectRequestBody => {
+    const {
+        userId, 
+        project
+    } = body;
+    if (!isPositiveNumber(userId))
+        throw new Error("Invalid user id");
     return {
         userId,
-        projectForm: {
-            name,
-            description,
-            startDate,
-            endDate,
-            leaderId
-        }
+        projectForm: parseToProjectForm(project, false)
     };
+}
+export const parseToProjectFormToUpdate = (body: any): ProjectForm => {
+    return parseToProjectForm(body, true);
 }
 const parseToProjectId = (params: any, error: string) => {
     const { projectId } = params;
@@ -46,22 +63,24 @@ export const parseToProjectIdToDelete = (params: any): number => {
 export const parseToProjectIdToGetDetails = (params: any): number => {
     return parseToProjectId(params, "Invalid project id");
 }
-export const parseToUpdateProjectRequestBody = (body: any): ProjectForm => {
-    const {
-        id, name, description,
-        startDate, endDate, leaderId
-    } = body;
-    if (!isPositiveNumber(id) ||
-        !isLimitMaximString(name,255) || !isString(description) ||
-        !isPositiveNumber(startDate) || !isPositiveNumber(endDate) ||
-        !isPositiveNumber(leaderId))
-        throw new Error("Invalid form to update project");
-    return {
-        id,
-        name,
-        description,
-        startDate,
-        endDate,
-        leaderId
-    };
-}
+// export const parseToUpdateProjectRequestBody = (body: any): ProjectForm => {
+//     const {
+//         id, name, description,
+//         startDate, endDate, leaderId
+//     } = body;
+//     if (!isPositiveNumber(id) ||
+//         !isLimitMaximString(name, 255) || 
+//         !isLimitMaximString(description, 200)  ||
+//         !isPositiveNumber(startDate) || 
+//         !isPositiveNumber(endDate) ||
+//         !isPositiveNumber(leaderId))
+//         throw new Error("Invalid form to update project");
+//     return {
+//         id,
+//         name,
+//         description,
+//         startDate,
+//         endDate,
+//         leaderId
+//     };
+// }
