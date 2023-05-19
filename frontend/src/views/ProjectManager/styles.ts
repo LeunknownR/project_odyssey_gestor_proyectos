7c5cf676605e5e3a8612bee2a54651ee88ec492c
Manuel Rivera Becerra <leunknownr@gmail.com>
2023-05-18 22:33:04 -0500

    getUserId

--- frontend/src/views/ProjectManager/styles.ts
+++ frontend/src/views/ProjectManager/styles.ts
@@ -10,3 +10,17 @@ export const TemporalMain = styled.main`
     min-height: calc(100vh - 75px);
     margin-left: 75px;
 `;
+export const CloseButtonProjectForm = styled.div`
+    position: absolute;
+    top: 4%;
+    right: 2.5%;
+    color: var(--darkblue-2);
+    cursor: pointer;
+    .iconify {
+        width: max-content;
+        font-size: 34px;
+    }
+    .update {
+        color: var(--white-1);
+    }
+`;
