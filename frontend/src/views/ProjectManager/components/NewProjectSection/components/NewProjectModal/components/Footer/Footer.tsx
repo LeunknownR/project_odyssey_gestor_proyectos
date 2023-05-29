c0d49cdad4d9bac7724ed29eba5ac55a35766dc5
Ralf <kerwints6@gmail.com>
2023-05-28 23:01:43 -0500

    Cambiando diseño de bolas de usuarios

--- frontend/src/views/ProjectManager/components/NewProjectSection/components/NewProjectModal/components/Footer/Footer.tsx
+++ frontend/src/views/ProjectManager/components/NewProjectSection/components/NewProjectModal/components/Footer/Footer.tsx
@@ -2,10 +2,15 @@ import CustomButton from "src/components/CustomButton/CustomButton";
 import { Container } from "./styles";
 import { FooterProps } from "./types";
 
-const Footer = ({ registerProject }: FooterProps) => {
+const Footer = ({ registerProject, formIsCompleted }: FooterProps) => {
     return (
         <Container>
-            <CustomButton content="Crear" size="big" onClick={registerProject}/>
+            <CustomButton
+                content="Crear"
+                size="big"
+                onClick={registerProject}
+                disabled={!formIsCompleted()}
+            />
         </Container>
     );
 };
--- frontend/src/views/ProjectManager/components/UpdateProjectModal/components/Footer/Footer.tsx
+++ frontend/src/views/ProjectManager/components/UpdateProjectModal/components/Footer/Footer.tsx
@@ -1,13 +1,19 @@
+import { useState, useEffect } from "react";
 import CustomButton from "src/components/CustomButton/CustomButton";
 import { Container } from "./styles";
 import { FooterProps } from "./types";
 
-const Footer = ({ updateProject }: FooterProps) => {
+const Footer = ({ updateProject, form }: FooterProps) => {
+    const [btnDisabled, setBtnDisabled] = useState<boolean>(true);
+    useEffect(() => {
+        setBtnDisabled(!form.haveChanges() || !form.isCompleted());
+    }, [form]);
     return (
         <Container>
             <CustomButton
                 content="Actualizar"
                 size="big"
+                disabled={btnDisabled}
                 onClick={updateProject}
             />
         </Container>
