faf04381f0bd9466d49060802c647d74e2f07931
Ralf <kerwints6@gmail.com>
2023-05-30 22:57:22 -0500

    -

---
+++ frontend/src/views/TaskManager/TaskManager.tsx
@@ -0,0 +1,5 @@
+const TaskManager = () => {
+    return ( <h1>Hola</h1> );
+}
+ 
+export default TaskManager;
---
+++ frontend/src/views/TaskManager/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,7 @@
+import { Container } from "./styles";
+
+const TaskCard = () => {
+    return <Container></Container>;
+};
+
+export default TaskCard;
