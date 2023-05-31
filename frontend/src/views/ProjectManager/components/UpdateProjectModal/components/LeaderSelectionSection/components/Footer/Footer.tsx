3691b737aed0125fd8b2875af64c4f9a6f6a6d56
Ralf <kerwints6@gmail.com>
2023-05-30 19:23:36 -0500

    Arreglado modal de actualizar proyecto

--- frontend/src/views/ProjectManager/components/UpdateProjectModal/components/LeaderSelectionSection/components/Footer/Footer.tsx
+++ frontend/src/views/ProjectManager/components/UpdateProjectModal/components/LeaderSelectionSection/components/Footer/Footer.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import CustomButton from "src/components/CustomButton/CustomButton";
 import { Container } from "./styles";
 import { FooterProps } from "./types";
 import useMainContext from "src/utils/contexts/main-context/useMainContext";
 import ResponsiveButtons from "src/views/ProjectManager/components/NewProjectSection/components/NewProjectModal/components/ResponsiveButtons/ResponsiveButtons";
 
-const Footer = ({ updateProject, formIsCompleted, tabIdx, toPage }: FooterProps) => {
+const Footer = ({
+    updateProject,
+    form,
+    tabIdx,
+    toPage,
+}: FooterProps) => {
     const { isMobile } = useMainContext();
+    const [buttonIsDisabled, setButtonIsDisabled] = useState<boolean>(true);
+    useEffect(() => {
+        setButtonIsDisabled(!form.haveChanges() || !form.isCompleted());
+    }, [form]);
     return (
         <Container>
             {isMobile && <ResponsiveButtons tabIdx={tabIdx} toPage={toPage} />}
@@ -13,7 +23,7 @@ const Footer = ({ updateProject, formIsCompleted, tabIdx, toPage }: FooterProps)
                 content="Actualizar"
                 size={isMobile ? "small" : "big"}
                 onClick={updateProject}
-                disabled={!formIsCompleted()}
+                disabled={buttonIsDisabled}
             />
         </Container>
     );
