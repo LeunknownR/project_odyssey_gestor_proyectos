7e1f9c9a472d37e7f6cc6416ccb537c75c3a01ad
Torresito__ <diegoteodosio@gmail.com>
2023-06-04 03:34:17 -0500

    Update db - New sps  to sprint 2

--- database/db.sql
+++ database/db.sql
@@ -62,6 +62,7 @@ CREATE TABLE `project` (
     `description` VARCHAR(200),
     `creation_date` DATE NOT NULL,
     `state` ENUM('P', 'O', 'F') NOT NULL,  -- Pending - OnProgress - Finalized
+    `checked` BIT NOT NULL,
     `start_date` DATE NOT NULL,
     `end_date` DATE NOT NULL,
     `active` BIT NOT NULL DEFAULT 1,
@@ -108,18 +109,24 @@ CREATE TABLE `task_priority` (
     PRIMARY KEY (`id_task_priority`)
 );
 
+INSERT INTO `task_priority`(`id_task_priority`, `task_priority_name`, `url_image`)
+VALUES 
+    (1, "low", "/low.jpg"),
+    (2, "medium", "/medium.jpg"),
+    (3, "high", "/high.jpg");
+
 -- Tabla para guardar las tareas
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
     `id_task` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `task_name` VARCHAR(40) NOT NULL,
-    `description` VARCHAR(200),
+    `description` VARCHAR(200),  -- task_description 
     `deadline` DATE,
     `state` ENUM('P', 'O', 'F') NOT NULL,  -- Pending - OnProgress - Finalized
     `checked` BIT NOT NULL,
-    `id_task_priority` INT UNSIGNED NOT NULL,
+    `id_task_priority` INT UNSIGNED NULL,
     `id_project` INT UNSIGNED NOT NULL,
-    `id_responsable` INT UNSIGNED,
+    `id_responsable` INT UNSIGNED NULL,
     PRIMARY KEY (`id_task`),
     FOREIGN KEY (`id_task_priority`) REFERENCES `task_priority`(`id_task_priority`),
     FOREIGN KEY (`id_project`) REFERENCES `project`(`id_project`),
@@ -247,7 +254,11 @@ VALUES
     (23, 13, 18, 'PLD'),
     (24, 14, 19, 'PLD');
 
-
+INSERT INTO `task`(id_task, task_name, id_project)
+VALUES 
+    (1, "db | Stored procedures 'sprint-2'", 1),
+    (2, "backend | new logict with POO 'sprint-2'", 1),
+    (3, "frontend | dev responsive design to mobile 'sprint-2'", 1);
 
 -- --- [ STORED PROCEDUREs ] ------------------------------------------------------------
 -- SP para el login
@@ -703,4 +714,174 @@ BEGIN
 END //
 DELIMITER ;
 
--- MIGRACIÓN DE LUCID - dbdiagram.io
+
+
+-- new sp sprint 2
+
+-- SP para traer el detalle de un colaborador de proyecto
+DELIMITER //
+CREATE PROCEDURE `sp_get_project_table_detail`(
+    IN p_id_project INT,
+    IN p_id_user INT
+)
+BEGIN
+    SELECT
+        p.id_project,
+        p.project_name,
+        p.state AS "project_state",
+        phc.id_project_role
+    FROM project p
+    INNER JOIN project_has_collaborator phc ON p.id_project = phc.id_project
+    INNER JOIN user u ON phc.id_collaborator = u.id_user
+    WHERE p.id_project = p_id_project
+    AND phc.id_collaborator = p_id_user;
+END //
+DELIMITER ;
+
+-- SP para traer las imagenes de las prioridades
+DELIMITER //
+CREATE PROCEDURE `sp_get_task_priorities`()
+BEGIN
+    SELECT 
+        id_task_priority,
+        url_image
+    FROM task_priority;
+END //
+DELIMITER ;
+
+-- sp_get_project_task_list(
+-- 		p_id_project
+-- );
+
+-- SP para traer las imagenes de las prioridades
+DELIMITER //
+CREATE PROCEDURE `sp_create_task`(
+    IN p_id_project INT,
+    IN p_task_name VARCHAR(40),
+    IN p_id_collaborator INT
+)
+BEGIN
+    -- Validando si el collab existe en el proyecto
+    IF NOT EXISTS(
+        SELECT id_collaborator
+        FROM project_has_collaborator
+        WHERE id_project = p_id_project
+        AND id_collaborator = p_id_collaborator
+    ) THEN
+        -- Cuando el colaborador no está dentro del proyecto.
+        SELECT 'COLLAB_IS_NOT_IN_PROJECT' AS 'message';
+    ELSE
+        -- Creando tarea basica
+        INSERT INTO task(
+            task_name,
+            id_project
+        ) VALUES (
+            p_task_name,
+            p_id_project
+        );
+        SET @id_task = LAST_INSERT_ID();
+
+        -- Verificando si el colaborador es miembro
+        IF EXISTS(
+            SELECT id_project_role
+            FROM project_has_collaborator
+            WHERE id_collaborator = p_id_collaborator
+            AND id_project = p_id_project
+            AND id_project_role = "PMB"
+        ) THEN
+            -- Seteandole la tarea al miembro
+            UPDATE task
+            SET id_responsable = p_id_collaborator
+            WHERE id_task = @id_task;
+        END IF;
+        -- Cuando la creación de la tarea es exitosa.
+        SELECT 'SUCCESS' AS 'message';
+    END IF;
+END //
+DELIMITER ;
+
+-- sp_update_task(
+-- 		p_id_project,
+-- 		p_id_responsable,
+-- 		p_task_name,
+-- 		p_description,
+-- 		p_deadline,
+-- 		p_id_priority,
+-- 		p_new_subtask_list,
+-- 		p_subtask_id_list_to_be_deleted,
+-- p_id_collaborator
+-- );
+
+-- SP para eliminar una tarea
+DELIMITER //
+CREATE PROCEDURE `sp_delete_task`(
+    IN p_id_project INT,
+    IN p_id_task_to_be_deleted INT,
+    IN p_id_collaborator INT
+)
+BEGIN
+    -- Validando si el collab existe en el proyecto
+    IF NOT EXISTS(
+        SELECT id_collaborator
+        FROM project_has_collaborator
+        WHERE id_project = p_id_project
+        AND id_collaborator = p_id_collaborator
+    ) THEN
+        -- Cuando el colaborador no está dentro del proyecto.
+        SELECT 'COLLAB_IS_NOT_IN_PROJECT' AS 'message';
+    ELSE
+        IF NOT EXISTS (
+            SELECT id_responsable
+            FROM task
+            WHERE id_task = p_id_task_to_be_deleted
+            AND id_responsable = p_id_collaborator
+        ) THEN
+            -- Cuando el colaborador es miembro del proyecto y no es su tarea.
+            SELECT 'COLLAB_IS_PMB_AND_TASK_IS_NOT_HIM' AS 'message';
+        ELSE
+            -- Eliminando la tarea
+            DELETE FROM task
+            WHERE id_task = p_id_task_to_be_deleted;
+            -- Cuando la creación de la tarea es exitosa.
+            SELECT 'SUCCESS' AS 'message';
+        END IF;
+    END IF;
+END //
+DELIMITER ;
+
+-- SP para crear un comentario
+DELIMITER //
+CREATE PROCEDURE `sp_comment_to_task`(
+    IN p_id_project INT,
+    IN p_id_task INT,
+    IN p_comment VARCHAR(200),
+    IN p_id_collaborator INT
+)
+BEGIN
+    -- Validando si el collab existe en el proyecto
+    IF NOT EXISTS(
+        SELECT id_collaborator
+        FROM project_has_collaborator
+        WHERE id_project = p_id_project
+        AND id_collaborator = p_id_collaborator
+    ) THEN
+        -- Cuando el colaborador no está dentro del proyecto.
+        SELECT 'COLLAB_IS_NOT_IN_PROJECT' AS 'message';
+    ELSE
+        -- Insertando el nuevo comentario
+        INSERT INTO task_commet(
+            comment_content, 
+            comment_date, 
+            id_task, 
+            id_collaborator
+        ) VALUES (
+            p_comment,
+            NOW(),
+            p_id_task,
+            p_id_collaborator
+        );
+        -- Cuando la creación de la tarea es exitosa.
+        SELECT 'SUCCESS' AS 'message';
+    END IF;
+END //
+DELIMITER ;
