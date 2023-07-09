6e946a3ea6d7840d9e3ccc1d7268897821c5ff02
Manuel Rivera Becerra <leunknownr@gmail.com>
2023-07-08 19:13:35 -0500

    Avances db y backend     * Se integró primer nombre a project chat preview.

--- backend/src/entities/chats/chatMessage/formattedProjectChatMessage.ts
+++ backend/src/entities/chats/chatMessage/formattedProjectChatMessage.ts
@@ -12,11 +12,7 @@ export default class FormattedProjectChatMessages {
             .map(record => new ProjectChatMessage(record));
         this.collaborators = resultset.map(record => ({
             id: record["id_collaborator_project"],
-            firstName: record["collaborator_project_name"]
+            firstName: record["collaborator_project_first_name"]
         }));
-        // Se valida si todos los mensajes son Null
-        if (this.messages.every(message => message.message === null))
-            // Se asigna Null a Messages
-            this.messages = null;
     }
 }
