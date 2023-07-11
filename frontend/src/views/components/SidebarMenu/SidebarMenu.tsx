504c540c99c49c2116da67ca60eaf5624e5fed4a
Ralf <kerwints6@gmail.com>
2023-07-10 23:16:14 -0500

    Botón de Chat ubicado correctamente en el Sidebar

--- frontend/src/views/components/SidebarMenu/SidebarMenu.tsx
+++ frontend/src/views/components/SidebarMenu/SidebarMenu.tsx
@@ -2,22 +2,32 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 import { Sidebar, MenuList, MenuButton } from "./styles";
 import { SidebarMenuProps } from "./types";
 import UserAdministration from "../Header/components/UserAdministration/UserAdministration";
-import { MENU_LINKS } from "./constants";
 import ChatMenuButton from "./constants/ChatMenuButton";
+import { AbsolutePaths } from "src/config/absolutePaths";
 
 const SidebarMenu = ({ mainMenuButton }: SidebarMenuProps) => {
     return (
         <Sidebar>
             {mainMenuButton}
             <MenuList>
+                <MenuButton to={AbsolutePaths.Projects} end activeclassname="active">
+                    <span>
+                        <Icon icon="material-symbols:home-outline-rounded" />
+                    </span>
+                </MenuButton>
                 <ChatMenuButton />
-                {MENU_LINKS.map(({ to, icon }, idx) => (
+                <MenuButton to={AbsolutePaths.Settings} end activeclassname="active">
+                    <span>
+                        <Icon icon="uiw:setting" />
+                    </span>
+                </MenuButton>
+                {/* {MENU_LINKS.map(({ to, icon }, idx) => (
                     <MenuButton key={idx} to={to} end activeclassname="active">
                         <span>
                             <Icon icon={icon} />
                         </span>
                     </MenuButton>
-                ))}
+                ))} */}
             </MenuList>
             <UserAdministration isInSidebar />
         </Sidebar>
