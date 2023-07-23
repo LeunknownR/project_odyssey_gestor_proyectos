0fec21d3eff6d459d5e3414d81606f06a10379e1
Torresito__ <diegoteodosio@gmail.com>
2023-07-22 22:20:02 -0500

    Avances db:     * Se desarrollaro los siguientes sps:         - sp_delete_collaborator_by_id         - sp_update_photo_collaborator     * Se actualizo los siguientes sps:         - sp_get_project_task_board:             Ahora tambien se envia"responsible_active"

--- database/db.sql
+++ database/db.sql
@@ -929,6 +929,7 @@ BEGIN
             urt.user_name AS "responsible_name",
             urt.user_surname AS "responsible_surname",
             urt.url_photo AS "responsible_url_photo",
+            urt.active AS "responsible_active ",
             t.id_task_priority,
             t.deadline AS "task_deadline",
             st.id_subtask,
@@ -1900,6 +1901,85 @@ BEGIN
         SET userpass = p_new_userpass
         WHERE id_user = p_id_collaborator;
 
+        -- Mostrando el mensaje de exito
+        SELECT 'SUCCESS' AS 'message';
+    END IF;
+END //
+DELIMITER ;
+
+-- SP para eliminar a un colaborador
+DELIMITER //
+CREATE PROCEDURE `sp_delete_collaborator_by_id`(
+    IN p_id_collaborator INT,
+    OUT url_photo_to_destroy VARCHAR(100)
+)
+BEGIN
+    -- Desactivando al colaborador
+    UPDATE user
+    SET active = 0
+    WHERE id_user = id_collaborator;
+
+    -- Extrayendo la url_photo para su eliminación
+    SELECT url_photo INTO url_photo_to_destroy
+    FROM user
+    WHERE id_user = id_collaborator;
+END //
+DELIMITER ;
+
+-- SP para actualizar la foto de un colaborador
+DELIMITER //
+CREATE PROCEDURE `sp_update_photo_collaborator`(
+    IN p_id_collaborator INT,
+    IN p_url_photo VARCHAR(100),
+    OUT url_photo_to_destroy VARCHAR(100)
+)
+BEGIN
+    -- Validación para ver si el collaborator es admin_general
+    IF EXISTS(
+        SELECT * 
+        FROM user
+        WHERE id_user = p_id_collaborator
+        AND id_role = "GAD"
+    ) THEN
+        -- Mostrando el mensaje GENERAL_ADMIN_CONFLICT
+        SELECT 'GENERAL_ADMIN_CONFLICT' AS 'message';
+        -- Validación para ver si el username existe
+    ELSEIF EXISTS(
+        SELECT username 
+        FROM user
+        WHERE username = p_username 
+        AND username NOT IN (
+            SELECT username 
+            FROM user 
+            WHERE id_user = p_id_collaborator 
+            AND active = 1
+        ) AND active = 1
+    ) THEN
+        -- Mostrando el mensaje USERNAME_ALREADY_EXISTS
+        SELECT 'USERNAME_ALREADY_EXISTS' AS 'message';
+        -- Validación para ver si el emial existe
+    ELSEIF EXISTS(
+        SELECT email 
+        FROM user
+        WHERE email = p_email 
+        AND email NOT IN (
+            SELECT email 
+            FROM user 
+            WHERE id_user = p_id_collaborator
+            AND active = 1
+        ) AND active = 1
+    ) THEN
+        -- Mostrando el mensaje EMAIL_ALREADY_EXISTS
+        SELECT 'EMAIL_ALREADY_EXISTS' AS 'message';
+    ELSE
+        SELECT url_photo INTO url_photo_to_destroy
+        FROM user
+        WHERE id_user = p_id_collaborator;
+
+        -- Actualizando el collaborator
+        UPDATE user
+        SET url_photo = p_url_photo
+        WHERE id_user = p_id_collaborator;
         -- Mostrando el mensaje de exito
         SELECT 'SUCCESS' AS 'message';
     END IF;
